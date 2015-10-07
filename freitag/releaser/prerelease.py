7796afce818415bb2ef48c850f0b3c9051567294
Gil Forcada <gforcada@gnome.org>
2015-10-07 15:31:26 +0200

    zest.releaser plugin: check for strings not marked for translating

--- freitag/releaser/prerelease.py
+++ freitag/releaser/prerelease.py
@@ -3,6 +3,10 @@ from git import InvalidGitRepositoryError
 from git import Repo
 from zest.releaser.utils import ask
 
+import os
+import subprocess
+import sys
+
 
 def vcs_updated(data):
     try:
@@ -42,6 +46,26 @@ def vcs_updated(data):
             else:
                 repo.git.rebase('origin/develop')
                 print('Rebased master branch on top of develop.')
+
+
+def check_translations(data):
+    """Check that all strings are marked as translatable."""
+    if not os.path.exists('{0}/bin/i18ndude'.format(data['workingdir'])):
+        return
+
+    process = subprocess.Popen(
+        ['bin/i18ndude', 'find-untranslated', '-n', 'src/', ],
+        stdout=subprocess.PIPE
+    )
+    stdout, stderr = process.communicate()
+    if 'ERROR' not in stdout:
+        return
+
+    print(stdout)
+    msg = 'There are strings not marked as translatable, ' \
+          'do you want to continue?'
+    if not ask(msg, default=False):
+        sys.exit(1)
 
 
 def _checkout_branch(repo, branch):
