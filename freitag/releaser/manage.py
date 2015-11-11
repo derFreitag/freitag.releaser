bc04faedd367cddfb7afd70ec6312e479726537f
Gil Forcada <gforcada@gnome.org>
2015-11-11 12:15:02 +0100

    Skeleton of freitag_release commands

--- freitag/releaser/manage.py
+++ freitag/releaser/manage.py
@@ -6,6 +6,26 @@ from plone.releaser.manage import jenkins_report
 from plone.releaser.manage import set_package_version
 
 
+def release(path):
+    """Release the distribution found on the given path."""
+    pass
+
+
+def collect_changelog():
+    """Collect changes made on distributions between a commit time frame."""
+    pass
+
+
+def publish_cfg_files():
+    """Push buildout .cfg files on a remote server."""
+    pass
+
+
+def sync_batou():
+    """Update version pins on batou"""
+    pass
+
+
 class Manage(object):
 
     def __call__(self, **kwargs):
@@ -16,6 +36,10 @@ class Manage(object):
             checkPackageForUpdates,
             jenkins_report,
             set_package_version,
+            release,
+            collect_changelog,
+            publish_cfg_files,
+            sync_batou,
         ]
 
         parser.add_commands(commands)
