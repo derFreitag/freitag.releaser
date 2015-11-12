f6894f0fb8e962f0ae4f264adda64a5cb5d8484f
Gil Forcada <gforcada@gnome.org>
2015-11-12 12:54:43 +0100

    Refactor: turn release into a class
    This makes it easy to enhance, change and repurpose it later on.

--- freitag/releaser/manage.py
+++ freitag/releaser/manage.py
@@ -14,61 +14,85 @@ DISTRIBUTION = '\033[1;91m{0}\033[0m'
 BRANCH = PATH = '\033[1;30m{0}\033[0m'
 
 
-def release(path):
-    """Release the distribution found on the given path."""
-    distribution_name = path.split('/')[1]
+class ReleaseDistribution(object):
+    """Release a single distribution with zest.releaser
 
-    _check_master_branch()
-    _check_distribution_exists(path)
-    version = _release(path)
+    It does some QA checks before/after the actual release happens.
+    """
 
-    buildout = Buildout(
-        sources_file='develop.cfg',
-        checkouts_file='develop.cfg',
-    )
-    buildout.set_version(distribution_name, version)
+    #: system path where the distribution should be found
+    path = None
+    #: name of the distribution
+    name = None
+    #: git repository of the distribution
+    repo = None
 
+    #: parent repository which will be updated with the new release
+    parent_repo = None
+    #: plone.releaser.buildout.Buildout instance of the parent repository
+    buildout = None
 
-def _check_master_branch():
-    repo = Repo(os.path.curdir)
-    current_branch = repo.active_branch.name
+    def __init__(self, path):
+        self.path = path
+        self.name = path.split('/')[1]
 
-    if current_branch != 'master':
-        text = '{0} is not on master branch, but on {1}'
-        raise ValueError(
-            text.format(
-                DISTRIBUTION.format('zope repository'),
-                BRANCH.format(current_branch)
+    def __call__(self):
+        self._check_parent_branch()
+        self._check_distribution_exists()
+        self._zest_releaser()
+
+        self.buildout = Buildout(
+            sources_file='develop.cfg',
+            checkouts_file='develop.cfg',
+        )
+        self.buildout.set_version(self.name, self.get_version())
+
+    def _check_parent_branch(self):
+        self.parent_repo = Repo(os.path.curdir)
+        current_branch = self.parent_repo.active_branch.name
+
+        if current_branch != 'master':
+            text = '{0} is not on master branch, but on {1}'
+            raise ValueError(
+                text.format(
+                    DISTRIBUTION.format('zope repository'),
+                    BRANCH.format(current_branch)
+                )
             )
-        )
+
+    def _check_distribution_exists(self):
+        """Check that the folder exists"""
+        if not os.path.exists(self.path):
+            raise IOError(
+                'Path {0} does NOT exist'.format(PATH.format(self.path))
+            )
+
+    def _zest_releaser(self):
+        """Release the distribution"""
+        # remove arguments so zest.releaser is not confused
+        # will most probably *not* be fixed by zest.releaser itself:
+        # https://github.com/zestsoftware/zest.releaser/issues/146
+        original_args = sys.argv
+        sys.argv = ['']
+
+        # change to the distribution root folder
+        original_path = os.getcwd()
+        os.chdir(self.path)
+
+        fullrelease.main()
+
+        os.chdir(original_path)
+        sys.argv = original_args
+
+    def get_version(self):
+        self.repo = Repo(self.path)
+        return self.repo.tags[-1].name
 
 
-def _check_distribution_exists(path):
-    """Check that the folder exists"""
-    if not os.path.exists(path):
-        raise IOError(
-            'Path {0} does NOT exist'.format(PATH.format(path))
-        )
-
-
-def _release(path):
-    """Release the distribution"""
-    # remove arguments so zest.releaser is not confused
-    # will be fixed by https://github.com/zestsoftware/zest.releaser/issues/146
-    original_args = sys.argv
-    sys.argv = ['']
-
-    # change to the distribution root folder
-    original_path = os.getcwd()
-    os.chdir(path)
-
-    fullrelease.main()
-
-    os.chdir(original_path)
-    sys.argv = original_args
-
-    repo = Repo(path)
-    return repo.tags[-1].name
+def release(path):
+    """Release the distribution found on the given path"""
+    release_distribution = ReleaseDistribution(path)
+    release_distribution()
 
 
 def collect_changelog():
