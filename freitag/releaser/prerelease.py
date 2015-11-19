bfad8fc0187bd35335035cfe6fe26c4718c41471
Gil Forcada <gforcada@gnome.org>
2015-11-19 16:32:51 +0100

    Adapt prerelease to logging
    This module is different from the others as it is called by zest.releaser,
    thus no configuration needs to be done.

--- freitag/releaser/prerelease.py
+++ freitag/releaser/prerelease.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 from zest.releaser.utils import ask
 
+import logging
 import os
 import subprocess
 import sys
 
 
+logger = logging.getLogger(__name__)
+
+
 def check_translations(data):
-    """Check that all strings are marked as translatable."""
-    if not os.path.exists('{0}/bin/i18ndude'.format(data['workingdir'])):
+    """Check that all strings are marked as translatable.
+
+    :param data: information coming from zest.releaser
+    :type data: dict
+    """
+    path = '{0}/bin/i18ndude'.format(data['workingdir'])
+    if not os.path.exists(path):
+        msg = '{0} not found, no translation check will be done'
+        logger.debug(msg.format(path))
         return
 
     process = subprocess.Popen(
@@ -17,9 +28,10 @@ def check_translations(data):
     )
     stdout, stderr = process.communicate()
     if 'ERROR' not in stdout:
+        logger.debug('i18ndude: everything up to date')
         return
 
-    print(stdout)
+    logger.info(stdout)
     msg = 'There are strings not marked as translatable, ' \
           'do you want to continue?'
     if not ask(msg, default=False):
