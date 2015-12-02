558ae31c4e7b51639a51bc4ed1aaf0eef3a8862c
Gil Forcada <gforcada@gnome.org>
2015-12-02 17:55:33 +0100

    logging: remove its prefix
    This way the output is not cluttered and any multiline output doesn't look funy.

--- freitag/releaser/utils.py
+++ freitag/releaser/utils.py
@@ -18,9 +18,13 @@ logger = logging.getLogger(__name__)
 
 def configure_logging(debug):
     if debug:
-        logging.basicConfig(level=logging.DEBUG)
+        level = logging.DEBUG
     else:
-        logging.basicConfig(level=logging.INFO)
+        level = logging.INFO
+    logging.basicConfig(
+        level=level,
+        format='%(message)s'
+    )
 
 
 def update_branch(repo, branch):
